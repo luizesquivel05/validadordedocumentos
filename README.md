aa6fc767510a911de02a01fa992ada6672a0cc2f
luizesquivel05 <luizeduardpontesesquivel@gmail.com>
2022-10-28 15:41:07 -0300

    att

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # validadordedocumentos
-O presente site, alia conhecimentos básicos de html5, css3 e vanilha js, com o intuito de auxiliar a todes, para reconhecer se um documento é verídico, ou não. DESENVOLVIDO POR ESQUIZANDO, O PRESENTE SITE, EM SUA VERSÃO PRIMÁRIA (MVP), OBJETIVA APRESENTAR AO PÚBLICO UMA FERRAMENTA SIMPLES E OBJETIVA QUE FAZ A LEITURA DE DADOS E ENTREGA SE EQUIVALE A UM NÚMERO DE DOCUMENTO VÁLIDO.
+O presente site, alia conhecimentos básicos de html5, css3 e vanilhaJS, com o intuito de entregar a todes, um sistema confiável para dizer se um CEP, CPF ou CNPJ é verídico, ou não (e, para alguns casos, interpretá-los). DESENVOLVIDO POR ESQUIZANDO, O PRESENTE SITE, EM SUA VERSÃO PRIMÁRIA (MVP), OBJETIVA APRESENTAR AO PÚBLICO UMA FERRAMENTA SIMPLES E OBJETIVA QUE FAZ A LEITURA DE DADOS E ENTREGA SE EQUIVALE A UM NÚMERO DE DOCUMENTO VÁLIDO.
 
 ## Dicas para melhor aproveitamento:
-- Primeiro escolha o tipo de documento que você irá querer validar, clique em ver ele;
-- Segundo digite o seu documento para validação (somente números sem máscaras);
+- Primeiro escolha o tipo de documento que você irá querer validar, clique em ver ele (CPF - CNPJ - CEP);
+- Segundo digite o seu documento para validação (somente números sem máscaras ['xxxxxxxxxxx' ao invés de 'xxx.xxx.xxx-xx']);
 - Terceiro clique em enviar e aguarde um tempinho que nosso algoritmo irá fazer o processo de leitura e validação.
 - Quarto na parte de saída o nosso site irá apresentar o resultado da análise.
 
 ## Considerações técnicas do desenvolvedor:
-- Infelizmente, o site pode não responder como desejado em celulares, pois seu MVP foi planejado para desktop, recomendamos que use seu celular em https://luizesquivel05.github.io/validadordedocumentos/#cpf (para cpf) e https://luizesquivel05.github.io/validadordedocumentos/#cnpj (para CNPJ).
+- Infelizmente, o site pode não responder como desejado em celulares, pois seu MVP foi planejado para desktop, recomendamos que use seu celular em https://luizesquivel05.github.io/validadordedocumentos/#cpf (para cpf); https://luizesquivel05.github.io/validadordedocumentos/#cnpj (para CNPJ) https://luizesquivel05.github.io/validadordedocumentos/#cep (para CEP).
 - Em versões futuras, o design responsivo está planejado, nosso foco agora está em performatizar o algoritmo, visando alcançar seu melhor desempenho possível;
 - Pedimos desculpa, mas nossos desenvolvedores ainda estão em processo de aprendizado e pode ser que tenha alguns erros de ordem téecnica.
 
@@ -31,3 +31,9 @@ O presente site, alia conhecimentos básicos de html5, css3 e vanilha js, com o 
 - Terceiro verificamos o segundo dígito de segurança (somatório dos 13 primeiros dígitos pela sequência 6 5 4 3 2 9 8 7 6 5 4 3 2, depois multiplica por 10 e pega a razão da divisão por 11);
 - Quarto verificamos se os 2 últimos díigitos dados confere com os 2 de segurança.
 - Em um quinto momento, foi visto se o 11º dígito, caso seja 1 é entregue sede e caso seja 2, 3 ou 4 é filial. Se for um dígito diferente, então não é cnpj válido.
+
+## Como fizemos a validação do CEP?
+- Foi verificado se o número apresenta 8 dígitos (quantidade de dígitos do CEP).
+- O CEP, diferente do CNPJ ou CPF não possui dígitos de segurança.
+
+## Como fizemos a validação da conta?
--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+/* FUNÇÃO USADA PARA VALIDAÇÃO DO CPF */
 function lendoosdados(){
+
+    /* LER OS DADOS E ACESSAR A STRING */
     var pessoa = document.getElementById("lerdoc").value;
     document.getElementById("resp1").innerHTML = pessoa;
     var c1 = pessoa[0];
@@ -11,12 +14,16 @@ function lendoosdados(){
     var c8 = pessoa[7];
     var c9 = pessoa[8];
     var c10 = pessoa[9];
+
+    /* CÁLCULO PARA VALIDAÇÃO */
     aux = c1 * 10 + c2 * 9 + c3 * 8 + c4 * 7 + c5 * 6 + c6 * 5 + c7 * 4 + c8 * 3 + c9 * 2;
     aux2 = aux * 10;
     d1 = aux2 % 11;
     aux1 = c1 * 11 + c2 * 10 + c3 * 9 + c4 * 8 + c5 * 7 + c6 * 6 + c7 * 5 + c8 * 4 + c9 * 3 + c10 * 2;
     aux3 = aux1 * 10;
-    d2 = aux3 % 11;    
+    d2 = aux3 % 11;
+
+    /* VERIFICAÇÃO DE VALIDADE */
     if(pessoa[10] == undefined) { 
         document.getElementById("resp2").innerHTML = "não"; 
     } else if(pessoa[11] != undefined) { 
@@ -30,10 +37,13 @@ function lendoosdados(){
     }
 }
 
+/* FUNÇÃO PARA VALIDAÇÃO DO CNPJ */
 function lendoosdados2(){
+    /* LER OS CNPJ */
     var juridica = document.getElementById("lerdocJ").value;
     document.getElementById("resp4").innerHTML = juridica;
 
+    /* ACESSAR STRING */
     var j1 = juridica[0];
     var j2 = juridica[1];
     var j3 = juridica[2];
@@ -48,17 +58,29 @@ function lendoosdados2(){
     var j12 = juridica[11];
     var j13 = juridica[12];
 
+    /* CÁLCULO PARA VALIDAÇÃO */
     var auxJ = j1*5 + j2*4 + j3*3 + j4*2 + j5*9 + j6*8 + j7*7 + j8*6 + j9*5 + j10*4 + j11*3 + j12*2;
     var auxJ1 = auxJ % 11;
 
-    if(auxJ1 < 2) { var dJ1 = 0; } else { var dJ1 = 11 - auxJ1 }
+    /* VERIFICAR PRIMEIRO DÍGITO */
+    if(auxJ1 < 2) { 
+        var dJ1 = 0; 
+    } else { 
+        var dJ1 = 11 - auxJ1 
+    }
 
+    /* CÁLCULO PARA VALIDAR SEGUNDO DÍGITO */
     var auxJ2 = j1*6 + j2*5 + j3*4 + j4*3 + j5*2 + j6*9 + j7*8 + j8*7 + j9*6 + j10*5 + j11*4 + j12*3 + j13*2;
     var auxJ3 = auxJ2 % 11;
 
-    if(auxJ3 < 2) { var dJ2 = 0; } else { var dJ2 = 11 - auxJ3 }
+    /* VALIDAR SEGUNDO DÍGITO */
+    if(auxJ3 < 2) { 
+        var dJ2 = 0; 
+    } else { 
+        var dJ2 = 11 - auxJ3 
+    }
 
-
+    /* VALIDAÇÃO DO CNPJ */
     if(juridica[13] == undefined) { 
         document.getElementById("resp5").innerHTML = "não"; 
     } else if(juridica[14] != undefined) { 
@@ -71,5 +93,72 @@ function lendoosdados2(){
         document.getElementById("resp5").innerHTML = "não";
     }
 
-    if(dJ1 == juridica[12] && dJ2 == juridica[13] && juridica[11] == 1) {document.getElementById("resp6").innerHTML = "sede";} else if(dJ1 == juridica[12] && dJ2 == juridica[13] && (juridica[11] == 1 || juridica[11] == 2 || juridica[11] == 3)) {document.getElementById("resp6").innerHTML = "filial";}
+    /* INTERPRETAÇÃO DOS DADOS DO CNPJ */
+    if(dJ1 == juridica[12] && dJ2 == juridica[13] && juridica[11] == 1) {
+        document.getElementById("resp6").innerHTML = "sede";
+    } else if(dJ1 == juridica[12] && dJ2 == juridica[13] && (juridica[11] == 1 || juridica[11] == 2 || juridica[11] == 3)) {
+        document.getElementById("resp6").innerHTML = "filial";
+    }
+}
+
+/* FUNÇÃO VALIDAÇÃO DO CEP */
+function lendoosdados3(){
+    /* LER O CEP E ACESSAR STRING */
+    var endereco = document.getElementById("lerdocCEP").value;
+    document.getElementById("resp7").innerHTML = endereco;
+    var c1 = endereco[0];
+    var c2 = endereco[1];
+    var c3 = endereco[2];
+    var c4 = endereco[7];
+
+    /* VERIFICAR VALIDADE DO CEP. */
+    if(endereco>=01000000 && endereco<=99999999 && c4 != undefined) {
+        document.getElementById("resp8").innerHTML = "SIM";
+
+        if(c1 == 0 || c1 == 1 || c1 == 2 || c1 == 3) {
+            document.getElementById("resp9").innerHTML = "SUDESTE";
+
+            if(c1 == 0 || c1 == 1) {
+                document.getElementById("resp10").innerHTML = "SÃO PAULO";
+                if(c2 == 1 || c2 == 2 || c2 == 3 || c2 == 4 || c2 == 5){
+                    document.getElementById("resp11").innerHTML = "CAPITAL"; 
+                }else if(c2 == 6 || c2 == 7 || c2 == 8 || c2 == 9) {
+                    document.getElementById("resp11").innerHTML = "ÁREA METROPOLITANA"; 
+                }else if(c1 == 1 && c2 == 1){
+                    document.getElementById("resp11").innerHTML = "LITORAL";
+                } else {
+                    document.getElementById("resp11").innerHTML = "INTERIOR";
+                }
+            } else if(c1 == 2 && c2 != 9) {
+                document.getElementById("resp10").innerHTML = "RIO DE JANEIRO";
+                if(c2 == 0 || c2 == 1 || c2 == 2 || c2 == 3 ) {
+                    document.getElementById("resp11").innerHTML = "CAITAL";
+                }  else if(c2 == 4 || c2 == 5 || c2 == 6) {
+                    document.getElementById("resp11").innerHTML = "ÁREA METROPOLITANA"; 
+                } else {
+                    document.getElementById("resp11").innerHTML = "INTERIOR";
+                }
+            } else if(c1 == 2 && c2 == 9) {
+                document.getElementById("resp10").innerHTML = "ESPÍRITO SANTO";
+                if(c3 == 0) {
+                    document.getElementById("resp11").innerHTML = "CAITAL";
+                } else {
+                    document.getElementById("resp11").innerHTML = "INTERIOR";
+                }
+            } else if(c1 == 3) {
+                document.getElementById("resp10").innerHTML = "MINAS GERAIS";
+            }
+        } else if(c1 == 8 || c1 == 9 ) {
+            document.getElementById("resp9").innerHTML = "SUL";
+        } else if(c1 == 4 || c1 == 5 || c1 == 6 || c1 == 7) {
+            document.getElementById("resp9").innerHTML = "NORDESTE";
+        } else {
+            document.getElementById("resp9").innerHTML = "não foi possível detectar (descontinuidade de alguns estados, verifique o estado";
+        }
+    } else {
+        document.getElementById("resp8").innerHTML = "NÃO";
+        document.getElementById("resp9").innerHTML = "INVÁLIDO";
+        document.getElementById("resp10").innerHTML = "INVÁLIDO";
+        document.getElementById("resp11").innerHTML = "INVÁLIDO";
+    }
 }
